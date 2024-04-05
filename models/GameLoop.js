0bf6386ddbc89af8e474c9cba683143af24bdab3
Martin-Martinez-4 <martmartez4@gmail.com>
2024-04-04 20:30:31 -0700

    Just started creating the handleBattle method.

---
+++ models/GameLoop.js
@@ -0,0 +1,23 @@
+
+// Maybe Scence classes to move between world view and battle
+
+export class GameLoop{
+
+    // handle battle
+        // has to keep track of turn order
+        // adding and subtracting turns
+        // checking player hp and mp
+    handleBattle(playerTeamArray, enemyTeamArray){
+
+        // allocate turns
+        let playerTurns = playerTeamArray.length;
+        let enemyTurns = enemyTeamArray.length;
+        
+
+    }
+
+    // handle game over
+
+    // 
+
+}
